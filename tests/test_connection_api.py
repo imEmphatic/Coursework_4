b02b49569f1d841647d086c13281a7d8c35e9a30
Aleksey <imemphatic@yandex.ru>
2024-07-22 05:28:01 +0300

    add file test_connection_api

---
+++ tests/test_connection_api.py
@@ -0,0 +1,13 @@
+from src.connection_api import HH
+
+test_api = HH()
+
+def test_get_url():
+    """тест на верное подключение к hh.ru"""
+    assert test_api.get_url() == 'https://api.hh.ru/vacancies'
+def test_connection_api():
+    """проверка подключения"""
+    assert str(test_api.get_response(1, "python")) == '<Response [200]>'
+
+def test_get_vacancion():
+    assert type(test_api.get_vacancies(1, 'python')) == list
