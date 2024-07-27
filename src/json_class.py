554f17d62f509c326a087509bb987c6a259362b7
Aleksey <imemphatic@yandex.ru>
2024-07-27 17:00:47 +0300

    edit json_class.py

--- src/json_class.py
+++ src/json_class.py
@@ -2,13 +2,16 @@ import json
 from abc import ABC, abstractmethod
 from src.vacancy import Vacancy
 
+
 class AbstractJsonClass(ABC):
     @abstractmethod
     def write_data(self, vac):
         pass
+
     @abstractmethod
     def get_vacancies(self):
         pass
+
     @abstractmethod
     def delete_vacancy(self):
         pass
@@ -16,9 +19,11 @@ class AbstractJsonClass(ABC):
 
 class GetJson(AbstractJsonClass):
     """класс для работы с файлом vacancies.json"""
+
     def __init__(self, filename="vacancies.json"):
         """указываем файл для сохранения данных """
         self.__filename = f"data/{filename}"
+
     def write_data(self, vac):
         """записываем данные в файл"""
         with open(self.__filename, 'w') as f:
@@ -36,7 +41,6 @@ class GetJson(AbstractJsonClass):
                 url=vacancy['alternate_url'],
                 employer=vacancy['employer']['name']
             ))
-
         return vacancies
 
     def delete_vacancy(self):
