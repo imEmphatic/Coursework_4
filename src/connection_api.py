85ac975b04c9e4258e3ba18c0f0c930dca924e82
Aleksey <imemphatic@yandex.ru>
2024-07-27 16:57:09 +0300

    edit connection_api.py

--- src/connection_api.py
+++ src/connection_api.py
@@ -14,26 +14,25 @@ class AbstractApi(ABC):
 
 class HH(AbstractApi):
     """Класс для взаимодействия с API HeadHunter"""
-
     def __init__(self):
         self.__url = "https://api.hh.ru/vacancies"
     """Ссылка для получения БД"""
 
     def get_url(self):
+        """геттер для получения url и его возврата необходимый для теста"""
         return self.__url
-    """геттер для получения url и его возврата необходимый для теста"""
 
     def _get_response(self, per_page: int, text: str = ''):
+        """получение первичной информации о вакансиях с сайта"""
         self.per_page = per_page
         self.text = text
         params = {'text': self.text, 'page': 0, 'per_page': self.per_page}
         response = requests.get(self.__url, params)
         return response
-    """получение первичной информации о вакансиях с сайта"""
 
     def get_vacancies(self, per_page: int, text: str = ''):
+        """перевод информации в json формат"""
         self.per_page = per_page
         self.text = text
         response = self._get_response(self.per_page, self.text)
         return response.json()['items']
-    """перевод информации в json формат"""
